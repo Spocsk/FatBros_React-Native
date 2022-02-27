bf0c92120a3d0b78299bf7664285cc420fa73565
Dylan COUTO DE OLIVEIRA <dcoutodeoliveira@normandiewebschool.fr>
2022-02-27 22:43:57 +0100

    tv show hook added to project

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, View } from 'react-native';
 import Slider from './src/components/Slider';
+import Subtitle from './src/components/Subtitle';
 import useMovies from './src/hooks/useMovies';
+import useTvShows from './src/hooks/useTvShows'
 
 export default function App() {
 
   const movies = useMovies();
+  const tv = useTvShows();
+  console.log(tv)
 
   return (
     <SafeAreaView style={styles.view}>
-      <Slider data={movies}></Slider>
+      <View>
+        <Subtitle>À découvrir</Subtitle>
+        <Slider data={movies}></Slider>
+        <Subtitle>Top des séries</Subtitle>
+        <Slider data={tv}></Slider>
+      </View>
     </SafeAreaView>
   );
 }
@@ -16,5 +25,6 @@ export default function App() {
 const styles = StyleSheet.create({
   view: {
     flex: 1,
+    backgroundColor: 'rgb(31,27,26)',
   }
 });
