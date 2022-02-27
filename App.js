3029658811d2a059553611da3f559998f0bbef5a
Dylan COUTO DE OLIVEIRA <dcoutodeoliveira@normandiewebschool.fr>
2022-02-27 21:00:09 +0100

    restructuring the app.js file

--- App.js
+++ App.js
@@ -1,38 +1,15 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
-import getDiscover from './src/api/tmdb'
-import axios from 'axios'
-import {
-    API_BASE_URL,
-    API_BEARER_TOKEN
-} from '@env'
-import {
-  useQuery,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { StyleSheet, SafeAreaView } from 'react-native';
+import Slider from './src/components/Slider';
+import useMovies from './src/hooks/useMovies';
 
 export default function App() {
-  
-  const queryClient = new QueryClient();
-  
-  const fetchDiscoverMovies = async () => {
-    return await axios.get(`${API_BASE_URL}/discover/movie`, {
-      headers: {
-        Authorization: 'bearer ' + API_BEARER_TOKEN
-    }
-    })
-  }
-  const { data } = useQuery('discover', fetchDiscoverMovies)
+
+  const movies = useMovies();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaView style={styles.view}>
-        <Text>{data[0].id}</Text>
-      </SafeAreaView>
-      <ReactQueryDevtools initialIsOpen />
-    </QueryClientProvider>
+    <SafeAreaView style={styles.view}>
+      <Slider data={movies}></Slider>
+    </SafeAreaView>
   );
 }
 
