25aebbff6ccc108ffdafa180fa67289c3bc58ae5
Dylan COUTO DE OLIVEIRA <dcoutodeoliveira@normandiewebschool.fr>
2022-02-27 21:23:59 +0100

    add image to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,25 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { API_IMAGE_BASE_URL } from '@env';
 
-export default function Card({data}) {
+export default function Card({img}) {
 
   return (
     <View style={styles.card}>
-        <Text>{data}</Text>
+        <Image style={styles.img} source={{uri: `${API_IMAGE_BASE_URL}${img}`}}></Image>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
     card: {
-        width: 120,
-        height: 150,
-        backgroundColor: 'red',
+        width: 140,
+        height: 200,
         marginHorizontal: 10,
+        borderRadius: 15
+    },
+    img: {
+        width: '100%',
+        height: '100%',
         borderRadius: 15
     }
 });
