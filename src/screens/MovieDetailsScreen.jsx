1a129bbdc8b7f3795b4248fe9ebedffe845629e4
Dylan COUTO DE OLIVEIRA <dcoutodeoliveira@normandiewebschool.fr>
2022-03-01 01:13:37 +0100

    Ajout d'une image de fond et du bouton de retour

--- src/screens/MovieDetailsScreen.jsx
+++ src/screens/MovieDetailsScreen.jsx
@@ -1,7 +1,27 @@
-import { View, Text } from 'react-native'
+import { SafeAreaView, StyleSheet, Image, View} from 'react-native'
+import PreviousButton from '../components/Button/PreviousButton'
+import { API_IMAGE_BASE_URL } from '@env'
 
-export default function MovieDetailsScreen({route}) {
+export default function MovieDetailsScreen({navigation, route}) {
+    const data = route.params.data
     return (
-        <Text>{route.params.id}</Text>
+        <View style={styles.container}>
+            <PreviousButton onClick={() => navigation.navigate('Home')}></PreviousButton>
+            <Image style={styles.img} source={{uri: `${API_IMAGE_BASE_URL}${data.backdrop_path}`}}></Image>
+        </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'relative',
+        height: 550
+    },
+    img: {
+        zIndex: 0,
+        height: '100%',
+        zIndex: 1,
+        resizeMode: 'cover',
+    }
+  });
